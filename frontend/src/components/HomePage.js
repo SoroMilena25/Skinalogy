36e8a0e832ef6c9c6410fff9fe8ab7dea44c54d0
SoroMilena25 <soro.milena25@gmail.com>
2025-06-20 17:57:13 +0200

    page home finalisé

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -27,33 +27,60 @@ const HomePage = () => {
       {/* Section TOP DU MOMENT */}
       <section className="top-moment">
         <h2 className="section-title">TOP DU MOMENT</h2>
-        <div className="products-grid">
-          <div className="product-card">
+        <div className="products-gridHome">
+          {/*<div className="product-cardHome">
             <div className="product-image">
               <div className="placeholder-img"></div>
-              <button className="add-btn">+</button>
+              <button className="add-btnHome">+</button>
             </div>
-            <h3 className="product-title">Crème hydratante smooth</h3>
-            <p className="product-price">14.50€</p>
+            <h3 className="product-titleHome">Crème hydratante smooth</h3>
+            <p className="product-priceHome">14.50€</p>
           </div>
           
-          <div className="product-card">
+          <div className="product-cardHome">
             <div className="product-image">
               <div className="placeholder-img"></div>
-              <button className="add-btn">+</button>
+              <button className="add-btnHome">+</button>
             </div>
-            <h3 className="product-title">Sérum - Acide hyaluronique</h3>
-            <p className="product-price">12.75€</p>
+            <h3 className="product-titleHome">Sérum - Acide hyaluronique</h3>
+            <p className="product-priceHome">12.75€</p>
           </div>
           
-          <div className="product-card">
+          <div className="product-cardHome">
             <div className="product-image">
               <div className="placeholder-img"></div>
-              <button className="add-btn">+</button>
+              <button className="add-btnHome">+</button>
             </div>
-            <h3 className="product-title">Sérum - Vitamine C + B</h3>
-            <p className="product-price">8.45€</p>
+            <h3 className="product-titleHome">Sérum - Vitamine C + B</h3>
+            <p className="product-priceHome">8.45€</p>
+          </div>*/}
+          <div className="product-cardHome title-top">
+            <h3 className="product-titleHome">Crème hydratante smooth</h3>
+            <div className="product-imageHome">
+              <div className="placeholder-imgHome"></div>
+              <button className="add-btnHome">+</button>
+            </div>
+            <p className="product-priceHome">14.50€</p>
           </div>
+
+          <div className="product-cardHome title-top">
+            <h3 className="product-titleHome">Crème hydratante smooth</h3>
+            <div className="product-imageHome">
+              <div className="placeholder-imgHome"></div>
+              <button className="add-btnHome">+</button>
+            </div>
+            <p className="product-priceHome">14.50€</p>
+          </div>
+
+          <div className="product-cardHome title-top">
+            <h3 className="product-titleHome">Crème hydratante smooth</h3>
+            <div className="product-imageHome">
+              <div className="placeholder-imgHome"></div>
+              <button className="add-btnHome">+</button>
+            </div>
+            <p className="product-priceHome">14.50€</p>
+          </div>
+          
         </div>
       </section>
 
@@ -74,30 +101,40 @@ const HomePage = () => {
       <section className="beauty-basics">
         <div className="content-wrapper">
           <div className="text-content">
-            <h2 className="section-title">BEAUTY BASICS</h2>
-            <h3>Quel est l'utilité d'une crème solaire ?</h3>
-            <p>
-              La <strong>crème solaire</strong> protège ta peau des effets 
-              nocifs du soleil, en particulier des rayons UV 
-              (UVA et UVB). Elle <strong>prévient le vieillissement 
-              prématuré, les taches pigmentaires, les 
-              coups de soleil</strong>... et surtout, elle réduit le 
-              <strong>risque de cancer de la peau</strong>. C'est LE geste 
-              incontournable, été comme hiver, pour 
-              <strong>préserver l'éclat et la santé de ta peau</strong>.
-            </p>
+            <div>
+              <h2 className="section-title">BEAUTY BASICS</h2>
+              <h3>Quel est l'utilité d'une crème solaire ?</h3>
+              <p>
+                La <strong>crème solaire</strong> protège ta peau des effets 
+                nocifs du soleil, en particulier des rayons UV 
+                (UVA et UVB). Elle <strong>prévient le vieillissement 
+                prématuré, les taches pigmentaires, les 
+                coups de soleil</strong>... et surtout, elle réduit le 
+                <strong>risque de cancer de la peau</strong>. C'est LE geste 
+                incontournable, été comme hiver, pour 
+                <strong>préserver l'éclat et la santé de ta peau</strong>.
+              </p>
+            </div>
             <a href="#" className="beauty-link">
               &gt;&gt; Cliquez ici pour d'autres astuces beautés
             </a>
           </div>
           <div className="product-showcase">
-            <div className="product-card">
-              <div className="product-image">
-                <div className="placeholder-img"></div>
-                <button className="add-btn">+</button>
+            {/*<div className="product-cardHome">
+              <div className="product-imageHome">
+                <div className="placeholder-imgHome"></div>
+                <button className="add-btnHome">+</button>
               </div>
-              <h3 className="product-title">Crème solaire - SPF 50++</h3>
-              <p className="product-price">9.75€</p>
+              <h3 className="product-titleHome">Crème solaire - SPF 50++</h3>
+              <p className="product-priceHome">9.75€</p>
+            </div>*/}
+            <div className="product-cardHomeTips title-top">
+              <h3 className="product-titleHome">Crème solaire - SPF 50++</h3>
+              <div className="product-imageHome">
+                <div className="placeholder-imgHome"></div>
+                <button className="add-btnHome">+</button>
+              </div>
+              <p className="product-priceHome">9.75€</p>
             </div>
           </div>
         </div>
@@ -105,14 +142,15 @@ const HomePage = () => {
 
       {/* Section Idées Cadeaux */}
       <section className="gift-ideas">
-        <h2 className="section-title-white">IDÉES CADEAUX</h2>
+        <h2 className="section-title white">IDÉES CADEAUX</h2>
         <p className="section-subtitle">Le fête des mères approches à grands pas !</p>
         
+        {/* Grille de cartes cadeaux 
         <div className="gift-grid">
           <div className="gift-card">
             <div className="gift-image">
-              <div className="placeholder-img"></div>
-              <button className="add-btn">+</button>
+              <div className="placeholder-imgHome"></div>
+              <button className="add-btnHome">+</button>
             </div>
             <h3 className="gift-title">Coffret - Soins anti-âge</h3>
             <p className="gift-price">38.50€</p>
@@ -120,13 +158,34 @@ const HomePage = () => {
           
           <div className="gift-card">
             <div className="gift-image">
-              <div className="placeholder-img"></div>
-              <button className="add-btn">+</button>
+              <div className="placeholder-imgHome"></div>
+              <button className="add-btnHome">+</button>
             </div>
             <h3 className="gift-title">Coffret - Routine complète florale</h3>
             <p className="gift-price">45.85€</p>
           </div>
+        </div>*/}
+
+        <div className="products-gridHome">
+          <div className="product-cardHome title-top gift">
+            <h3 className="product-titleHome">Crème hydratante smooth</h3>
+            <div className="product-imageHome">
+              <div className="placeholder-imgHome"></div>
+              <button className="add-btnHome white-btn">+</button>
+            </div>
+            <p className="product-priceHome">14.50€</p>
+          </div>
+          <div className="product-cardHome title-top gift">
+            <h3 className="product-titleHome">Crème hydratante smooth</h3>
+            <div className="product-imageHome">
+              <div className="placeholder-imgHome"></div>
+              <button className="add-btnHome white-btn">+</button>
+            </div>
+            <p className="product-priceHome">14.50€</p>
+          </div>
         </div>
+
+        
       </section>
 
       {/* Footer */}
